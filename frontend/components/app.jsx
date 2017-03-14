c10ca319b7d604c0efaaaab751bfe7849a39294c
Daniel <daniel.j.fletcher2@gmail.com>
2017-03-14 14:51:50 -0700

    Create skeletons for components: App and Root, and set up initial React Route

---
+++ frontend/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = ({ children }) => (
+  <div>
+    <h1>are there planets?</h1>
+    { children }
+  </div>
+);
+
+export default App;
---
+++ frontend/entry.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  // const preloadedState = { session: { currentUser: window.currentUser } };
+  // const store = window.currentUser ? configureStore(preloadedState) : configureStore();
+  const store = configureStore();
+  window.store = store;
+
+  ReactDOM.render(<Root store={ store } />, root);
+});
+
+// <h1>plaaaaaaaaanets</h1>
