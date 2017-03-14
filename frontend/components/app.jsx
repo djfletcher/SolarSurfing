8ad523b6b5a2f810c82bbfa1952298b7f45d7cd0
Daniel <daniel.j.fletcher2@gmail.com>
2017-03-14 15:43:34 -0700

    Change version of react-router to 3.x in package.json

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,7 +3,7 @@ import HeaderContainer from './header/header_container';
 
 const App = ({ children }) => (
   <div>
-    <h1>are there planets?</h1>
+    <h1>plaaaaaaaaanets</h1>
     <HeaderContainer />
     { children }
   </div>
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -5,12 +5,10 @@ import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  // const preloadedState = { session: { currentUser: window.currentUser } };
-  // const store = window.currentUser ? configureStore(preloadedState) : configureStore();
-  const store = configureStore();
+  const preloadedState = { session: { currentUser: window.currentUser } };
+  const store = window.currentUser ? configureStore(preloadedState) : configureStore();
+  // const store = configureStore();
   window.store = store;
 
   ReactDOM.render(<Root store={ store } />, root);
 });
-
-// <h1>plaaaaaaaaanets</h1>
