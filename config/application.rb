43c06763822dd5efc43c554aaddafbcdb0e7e472
Daniel <daniel.j.fletcher2@gmail.com>
2017-03-14 16:58:59 -0700

    add line 14 camelize to application.rb and changed params in Join form to snake case

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module SolarSurfing
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    Jbuilder.key_format camelize: :lower
   end
 end
