1a1643b6fd5e1f646ed258e8e9157acfc487072e
Daniel <daniel.j.fletcher2@gmail.com>
2017-03-14 11:47:38 -0700

    Write CreateUsers migration nested under api

---
+++ db/migrate/20170314184412_create_api_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.string :image_url, null: false
+      t.text :bio, null: false
+      t.integer :planet_id, null: false
+
+      t.timestamps
+    end
+    add_index :users, :username, unique: true
+    add_index :users, :session_token, unique: true
+  end
+end
