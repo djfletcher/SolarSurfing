28fd0f11f1935b591914e6fe15c337cf4c34ac3d
Daniel <daniel.j.fletcher2@gmail.com>
2017-03-16 16:55:26 -0700

    Initial styling for PlanetShow

--- frontend/components/planets/planet_show.jsx
+++ frontend/components/planets/planet_show.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
 
 class PlanetShow extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
   componentDidMount() {
     this.props.requestPlanet();
   }
 
   render() {
+    const background = { backgroundImage: `url(${this.props.imageUrl})` };
     return(
-      <div>
-        <ul>
-          <li>ID: { this.props.id }</li>
-          <li>Name: { this.props.name }</li>
-          <li>Description: { this.props.description }</li>
-          <li>Image Url: { this.props.imageUrl }</li>
-        </ul>
+      <div className="planet-show" style={ background }>
+        <h1>{ this.props.name }</h1>
+        <p>{ this.props.description }</p>
       </div>
     );
   }
