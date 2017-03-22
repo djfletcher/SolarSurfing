856bb184b44ac25fa334f4bb958e88bafba79de9
Daniel <daniel.j.fletcher2@gmail.com>
2017-03-21 21:16:42 -0700

    Copy, save, and comment out current Review Form render method

--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -115,3 +115,66 @@ class ReviewForm extends React.Component {
 
 
 export default ReviewForm;
+
+// render() {
+// return(
+//   <div className="review-form-container">
+//     <h2>Leave { this.props.hostUsername } a Review</h2>
+//     { this.renderErrors() }
+//     <form
+//       name="reviewForm"
+//       onSubmit={ this.handleSubmit }
+//       className="review-form"
+//     >
+//       <input
+//         type="text"
+//         className="review-form-body"
+//         value={ this.state.body }
+//         onChange={ this.updateBody }
+//         placeholder="The perfect host!"
+//       />
+//       <div className="review-form-radio-group-and-submit">
+//         <div className="review-form-radio-group">
+//           <input
+//             type="radio"
+//             name="rating"
+//             onChange={ this.updateRating }
+//             value="1"
+//             checked={ this.state.rating === '1' }
+//           />
+//           <input
+//             type="radio"
+//             name="rating"
+//             onChange={ this.updateRating }
+//             value="2"
+//             checked={ this.state.rating === '2' }
+//           />
+//           <input
+//             type="radio"
+//             name="rating"
+//             onChange={ this.updateRating }
+//             value="3"
+//             checked={ this.state.rating === '3' }
+//           />
+//           <input
+//             type="radio"
+//             name="rating"
+//             onChange={ this.updateRating }
+//             value="4"
+//             checked={ this.state.rating === '4' }
+//           />
+//           <input
+//             type="radio"
+//             name="rating"
+//             onChange={ this.updateRating }
+//             value="5"
+//             checked={ this.state.rating === '5' }
+//           />
+//         </div>
+//
+//         <Button type="submit" bsStyle="primary">Submit</Button>
+//       </div>
+//     </form>
+//   </div>
+// );
+// }
