5d6981ac0458f4b953a03e1748b9a9dfc32f4e53
Daniel <daniel.j.fletcher2@gmail.com>
2017-03-22 14:05:38 -0700

    Create Splash page and initial styling

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const Splash = ({ children }) => (
-  <div>I AM SPLASH PAGE</div>
+  <div className="splash-container">
+    <div className="splash-background top">
+      <aside className="image-content">
+        <h1>Stay with Locals and Meet Travelers</h1>
+        <Button bsStyle="primary">Start Exploring</Button>
+      </aside>
+    </div>
+    <div className="splash-background middle">
+      <aside className="image-content">
+        <h1>Explore the Solar System</h1>
+        <Button bsStyle="primary">How It Works</Button>
+      </aside>
+      <div className="content-box">
+        <div className="connected-with">
+          <h1>Alien 1 connected with Alien 2</h1>
+        </div>
+      </div>
+    </div>
+    <div className="splash-background bottom">
+      <div className="content-box">
+        <div className="connected-with">
+          <h1>Alien 1 connected with Alien 2</h1>
+        </div>
+      </div>
+      <aside className="image-content">
+        <h1>Meet Groovy Extraterrestrials</h1>
+        <Button bsStyle="primary">Safety First</Button>
+      </aside>
+    </div>
+  </div>
 );
 
 export default Splash;
