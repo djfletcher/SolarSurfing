c9f869d3147694ed43043ebefbfc652b811ac0bb
Daniel <daniel.j.fletcher2@gmail.com>
2017-03-22 18:34:33 -0700

    Strip down CSS and edit classNames and ID's in HTML for OrbitalMap

--- frontend/components/orbital_map/orbital_map.jsx
+++ frontend/components/orbital_map/orbital_map.jsx
@@ -5,15 +5,15 @@ const OrbitalMap = () => (
   <section className="orbital-map-container">
 
   	<ul className="solarsystem">
-  	  <li className="sun"><a href="#sun"><span>Sun</span></a></li>
-  	  <li className="mercury"><a href="#mercury"><span>Mercury</span></a></li>
-  	  <li className="venus"><a href="#venus"><span>Venus</span></a></li>
-  	  <li className="earth"><a href="#earth"><span>Earth<span className="moon"> &amp; Moon</span></span></a></li>
-  	  <li className="mars"><a href="#mars"><span>Mars</span></a></li>
-  	  <li className="jupiter"><a href="#jupiter"><span>Jupiter</span></a></li>
-  	  <li className="saturn"><a href="#saturn"><span>Saturn &amp; <span className="ring">Ring</span></span></a></li>
-  	  <li className="uranus"><a href="#uranus"><span>Uranus</span></a></li>
-  	  <li className="neptune"><a href="#neptune"><span>Neptune</span></a></li>
+  	  <li className="sun-orbit"><a href="#sun"><span id="sun"></span></a></li>
+  	  <li className="mercury-orbit"><a href="#mercury"><span id="mercury"></span></a></li>
+  	  <li className="venus-orbit"><a href="#venus"><span id="venus"></span></a></li>
+  	  <li className="earth-orbit"><a href="#earth"><span id="earth"><span className="moon"></span></span></a></li>
+  	  <li className="mars-orbit"><a href="#mars"><span id="mars"></span></a></li>
+  	  <li className="jupiter-orbit"><a href="#jupiter"><span id="jupiter"></span></a></li>
+  	  <li className="saturn-orbit"><a href="#saturn"><span id="saturn"></span><span className="ring"></span></a></li>
+  	  <li className="uranus-orbit"><a href="#uranus"><span id="uranus"></span></a></li>
+  	  <li className="neptune-orbit"><a href="#neptune"><span id="neptune"></span></a></li>
   	</ul>
 
   </section>
