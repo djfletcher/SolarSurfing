c066d46481a7a3480753d74fa1a392e8b76b68be
Daniel <daniel.j.fletcher2@gmail.com>
2017-03-23 15:11:19 -0700

    Complete styling for explore-and-num-travelers-container

--- frontend/components/orbital_map/orbital_map_slider.jsx
+++ frontend/components/orbital_map/orbital_map_slider.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 // import ReactBootstrapSlider from 'react-bootstrap-slider';
 import Rheostat from 'rheostat';
 import NumericInput from 'react-numeric-input';
+import { Button, DropdownButton, MenuItem } from 'react-bootstrap';
 
 class OrbitalMapSlider extends React.Component {
   constructor(props) {
@@ -11,6 +12,7 @@ class OrbitalMapSlider extends React.Component {
     this.onChange = this.onChange.bind(this);
     this.onValuesUpdated = this.onValuesUpdated.bind(this);
     this.handleNumTravelers = this.handleNumTravelers.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
   }
 
   highlightPlanet(e) {
@@ -70,6 +72,10 @@ class OrbitalMapSlider extends React.Component {
     this.setState({ numTravelers: valueAsNumber });
   }
 
+  handleSearch() {
+
+  }
+
   render() {
     const min = 0;
     const max = 400;
@@ -96,63 +102,86 @@ class OrbitalMapSlider extends React.Component {
     };
 
     return (
-      <div className="search-bar-container">
-        <div className="search-bar-num-travelers">
-          <p>Travelers:</p>
-          <NumericInput
-            min={ 1 }
-            max={ 12 }
-            value={ this.state.numTravelers }
-            onChange={ this.handleNumTravelers } />
-        </div>
-        <div>
-          <p>Year of Travel: { value }</p>
-        </div>
-        <div className="rheostat-container">
-          <Rheostat
-            min={ min }
-            max={ max }
-            values={ [value] }
-            onChange={ this.onChange }
-            onValuesUpdated={ this.onValuesUpdated }
-          />
-        </div>
-      <section className="orbital-map-container">
+      <div className="orbital-map-and-search-container">
+        <section className="search-container">
+          <ul className="explore-and-num-travelers-container">
+            <li className="explore">
+              <DropdownButton
+                title="Explore"
+                id="1">
+                <MenuItem eventKey="mercury">Mercury</MenuItem>
+                <MenuItem eventKey="venus">Venus</MenuItem>
+                <MenuItem eventKey="earth">Earth</MenuItem>
+                <MenuItem eventKey="mars">Mars</MenuItem>
+                <MenuItem eventKey="jupiter">Jupiter</MenuItem>
+                <MenuItem eventKey="saturn">Saturn</MenuItem>
+                <MenuItem eventKey="uranus">Uranus</MenuItem>
+                <MenuItem eventKey="neptune">Neptune</MenuItem>
+              </DropdownButton>
+            </li>
+            <li className="num-travelers">
+              <p>Travelers:</p>
+              <NumericInput
+                min={ 1 }
+                max={ 12 }
+                value={ this.state.numTravelers }
+                onChange={ this.handleNumTravelers } />
+            </li>
+            <li>
+              <Button bsStyle="primary" onClick={ this.handleSearch }>Search</Button>
+            </li>
+          </ul>
+          <div className="travel-dates">
+            <div>
+              <p>Year of Travel: { value }</p>
+            </div>
+            <div className="rheostat-container">
+              <Rheostat
+                min={ min }
+                max={ max }
+                values={ [value] }
+                onChange={ this.onChange }
+                onValuesUpdated={ this.onValuesUpdated }
+              />
+            </div>
+          </div>
+        </section>
+        <section className="orbital-map-container">
 
-        <ul className="solarsystem">
-          <li className="sun-orbit">
-            <a href="#sun"><span id="sun"></span></a>
-          </li>
-          <li className="mercury-orbit" style={ orbitalPeriod("mercury") }>
-            <a href="#mercury"><span id="mercury"></span></a>
-          </li>
-          <li className="venus-orbit" style={ orbitalPeriod("venus") }>
-            <a href="#venus"><span id="venus"></span></a>
-          </li>
-          <li className="earth-orbit" style={ orbitalPeriod("earth") }>
-            <a href="#earth"><span id="earth">
-              <span className="moon" style={ orbitalPeriod("moon") }></span>
-            </span></a>
-          </li>
-          <li className="mars-orbit" style={ orbitalPeriod("mars") }>
-            <a href="#mars"><span id="mars"></span></a>
-          </li>
-          <li className="jupiter-orbit" style={ orbitalPeriod("jupiter") }>
-            <a href="#jupiter"><span id="jupiter"></span></a>
-          </li>
-          <li className="saturn-orbit" style={ orbitalPeriod("saturn") }>
-            <a href="#saturn"><span id="saturn"></span>
-            <span className="ring"></span></a>
-          </li>
-          <li className="uranus-orbit" style={ orbitalPeriod("uranus") }>
-            <a href="#uranus"><span id="uranus"></span></a>
-          </li>
-          <li className="neptune-orbit" style={ orbitalPeriod("neptune") }>
-            <a href="#neptune"><span id="neptune"></span></a>
-          </li>
-        </ul>
+          <ul className="solarsystem">
+            <li className="sun-orbit">
+              <a href="#sun"><span id="sun"></span></a>
+            </li>
+            <li className="mercury-orbit" style={ orbitalPeriod("mercury") }>
+              <a href="#mercury"><span id="mercury"></span></a>
+            </li>
+            <li className="venus-orbit" style={ orbitalPeriod("venus") }>
+              <a href="#venus"><span id="venus"></span></a>
+            </li>
+            <li className="earth-orbit" style={ orbitalPeriod("earth") }>
+              <a href="#earth"><span id="earth">
+                <span className="moon" style={ orbitalPeriod("moon") }></span>
+              </span></a>
+            </li>
+            <li className="mars-orbit" style={ orbitalPeriod("mars") }>
+              <a href="#mars"><span id="mars"></span></a>
+            </li>
+            <li className="jupiter-orbit" style={ orbitalPeriod("jupiter") }>
+              <a href="#jupiter"><span id="jupiter"></span></a>
+            </li>
+            <li className="saturn-orbit" style={ orbitalPeriod("saturn") }>
+              <a href="#saturn"><span id="saturn"></span>
+              <span className="ring"></span></a>
+            </li>
+            <li className="uranus-orbit" style={ orbitalPeriod("uranus") }>
+              <a href="#uranus"><span id="uranus"></span></a>
+            </li>
+            <li className="neptune-orbit" style={ orbitalPeriod("neptune") }>
+              <a href="#neptune"><span id="neptune"></span></a>
+            </li>
+          </ul>
 
-      </section>
+        </section>
       </div>
     );
   }
