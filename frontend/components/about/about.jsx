3ce3aabd054fa97ad3ecaa02b7db664d1a2f1847
Daniel <daniel.j.fletcher2@gmail.com>
2017-03-24 11:02:10 -0700

    Complete styling for site info pages

--- frontend/components/about/about.jsx
+++ frontend/components/about/about.jsx
@@ -25,8 +25,8 @@ const About = ({ location }) => (
     </div>
     <div className="about-container">
       <h1>About SolarSurfing</h1>
-      <p>SolarSurfing is created by me, Daniel Fletcher, as an exercise in React, Redux, and Rails. The project was motivated, at some level, I suppose, by a desire to go to space; but since I can't do it quite yet I've settled for making this groovy site instead.</p>
-      <p>If you are looking for a fullstack engineer who has experience with Ruby, Rails, Javascript, React, Redux, HTML5, CSS3, Node, Java, and SQL (or someone who can rapidly learn another framework) please feel free to contact me. My github username is djfletcher, you may find my linkedin profile here, and my email is daniel.j.fletcher2@gmail.com. Happy SolarSurfing!</p>
+      <p>SolarSurfing is created by me, Daniel Fletcher, as an exercise in React, Redux, and Rails. The project was motivated, at some level, by a desire to go to space; but since I can't do it quite yet I've settled for making this groovy site instead.</p>
+      <p>If you are looking for a fullstack engineer who has experience with Ruby, Rails, Javascript, React, Redux, HTML5, CSS3, Node, Java, and SQL (or someone who can rapidly learn another framework) please feel free to contact me. My github username is <a target="_blank" href="https://github.com/djfletcher/SolarSurfing">djfletcher</a>, you may find my linkedin profile <a target="_blank" href="https://www.linkedin.com/in/daniel-fletcher-525727119/">here</a>, and my email is daniel.j.fletcher2@gmail.com. Happy SolarSurfing!</p>
     </div>
   </div>
 );
