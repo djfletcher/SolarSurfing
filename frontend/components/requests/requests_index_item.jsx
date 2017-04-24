682d1291048bdd2b885c163e4ee92a04117072cb
Daniel <daniel.j.fletcher2@gmail.com>
2017-04-24 12:24:12 -0700

    Change requests date to  word format instead of number formate

--- frontend/components/requests/requests_index_item.jsx
+++ frontend/components/requests/requests_index_item.jsx
@@ -2,9 +2,47 @@ import React from 'react';
 
 class RequestsIndexItem extends React.Component {
 
+  parseDate(date) {
+    const parseMonth = numericString => {
+      switch(numericString) {
+        case '01':
+        return 'January';
+        case '02':
+        return 'February';
+        case '03':
+        return 'March';
+        case '04':
+        return 'April';
+        case '05':
+        return 'May';
+        case '06':
+        return 'June';
+        case '07':
+        return 'July';
+        case '08':
+        return 'August';
+        case '09':
+        return 'September';
+        case '10':
+        return 'October';
+        case '11':
+        return 'November';
+        case '12':
+        return 'December';
+        default:
+        return 'January';
+      }
+    };
+
+    const year = date.slice(0, 4);
+    const month = parseMonth(date.slice(5, 7));
+    const day = date.slice(8, 10);
+    return `${month} ${day}, ${year}`;
+  }
+
   render() {
     const request = this.props.request;
-    const bookingDates = `${request.arriveDate} to ${request.departDate}`;
+    const bookingDates = `${this.parseDate(request.arriveDate)} to ${this.parseDate(request.departDate)}`;
     const title = this.props.type === "host" ?
       `Guest: ${request.guestName}` : `Host: ${request.hostName}`;
 
