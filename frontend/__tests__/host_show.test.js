b11b799c5480c476dc8a9a7e72dd555112825d31
Daniel <daniel.j.fletcher2@gmail.com>
2017-05-11 18:26:13 -0700

    Write tests for HostShow component

--- frontend/__tests__/host_show.test.js
+++ frontend/__tests__/host_show.test.js
@@ -1,21 +1,22 @@
 /* globals jest */
 
+// jest.mock('../components/reviews/reviews', () => <div></div>);
 jest.mock('../components/reviews/reviews', () => (
   () => ({ render: () => ( <div></div> ) })
 ));
+// jest.mock('../components/requests/request_form_container', () => (
+//   <div id='request-form-container'></div>
+// ));
 jest.mock('../components/requests/request_form_container', () => {
   // need to name function so we can query for it later
   return function RequestFormContainer() {
-    return { render: () => ( <div></div> ) }
+    return { render: () => ( <div id='request-form-container'></div> ) }
   }
 });
 
 import React from 'react';
 import { mount } from 'enzyme';
 import HostShowContainer from '../components/hosts/host_show_container';
-import HostShow from '../components/hosts/host_show';
-// import Reviews from '../components/reviews/reviews';
-// import RequestFormContainer from '../components/requests/request_form_container';
 import * as HostActions from '../actions/hosts_actions';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
@@ -23,27 +24,29 @@ import thunk from 'redux-thunk';
 const testHost = {
   id: 6,
   username: 'testHost',
-  planetId: 5,
   bio: 'I am a test host',
+  imageUrl: 'url',
+  planetId: 5,
   reviews: []
 };
+const session = { currentUser: { username: 'user', id: 4 } };
 const middlewares = [ thunk ];
 const mockStore = configureMockStore(middlewares);
-const testStore = mockStore({ hostShow: testHost });
+const testStore = mockStore({ hostShow: testHost, session });
 
 describe('HostShow', () => {
   let HostShowWrapper;
 
   beforeEach(() => {
       HostActions.requestHost = jest.fn(() => dispatch => {});
+      HostActions.removeReview = jest.fn(() => dispatch => {});
       HostShowWrapper = mount(
         <HostShowContainer store={testStore} />
-      ).find(HostShow);
+      ).find('#book-host-reviews-container');
     });
 
   it('correctly maps state to props', () => {
-    let { id, username, planetId, bio, reviews } = HostShowWrapper.props();
-    expect({ id, username, planetId, bio, reviews }).toEqual(Object.values(testHost));
+    expect(HostShowWrapper.props()).toEqual(Object.values(testHost));
   });
 
   it('correctly maps dispatch to props', () => {
@@ -55,4 +58,8 @@ describe('HostShow', () => {
     expect(HostActions.requestHost).toBeCalled();
   });
 
+  it('contains a RequestFormContainer component', () => {
+    const requestForm = HostShowWrapper.find('#request-form-container');
+    expect(requestForm.length).toBe(1);
+  });
 });
