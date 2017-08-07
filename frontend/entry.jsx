38155f539947b347a912c08f58312d6ff9ff5c88
Daniel <daniel.j.fletcher2@gmail.com>
2017-08-07 16:12:17 -0700

    Clean up entry file

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,16 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import {
-  RECEIVE_PLANETS, RECEIVE_SINGLE_PLANET, receivePlanets, receiveSinglePlanet, requestAllPlanets, requestSinglePlanet
-} from './actions/planets_actions';
-
-window.RECEIVE_PLANETS = RECEIVE_PLANETS;
-window.RECEIVE_SINGLE_PLANET = RECEIVE_SINGLE_PLANET;
-window.receivePlanets = receivePlanets;
-window.receiveSinglePlanet = receiveSinglePlanet;
-window.requestAllPlanets = requestAllPlanets;
-window.requestSinglePlanet = requestSinglePlanet;
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
